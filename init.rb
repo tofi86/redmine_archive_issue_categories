d9e837c2d2a405e535879718bcaa6e5700bd7e0f
Tobias Fischer <tobias.carl.fischer@gmail.com>
2015-03-01 15:41:40 +0100

    Version 0.3.0 (requires Redmine 3.0)
    A version for Redmine 2.6.x can be found in the branch `redmine/version-2.6`

--- init.rb
+++ init.rb
@@ -5,8 +5,8 @@ Redmine::Plugin.register :redmine_archive_issue_categories do
   name 'Redmine Archive Issue Categories'
   author 'Tobias Fischer'
   description "This Redmine plugin allows you to archive issue categories. This means, to deactivate and hide them from the user without deleting them. This is nice because it doesn't break old tickets"
-  version '0.2.0'
+  version '0.3.0'
   url 'https://github.com/tofi86/redmine_archive_categories'
   author_url 'https://github.com/tofi86'
-  requires_redmine :version_or_higher => '2.6.0'
+  requires_redmine :version_or_higher => '3.0.0'
 end
